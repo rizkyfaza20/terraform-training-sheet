c3fb003f5e622f3ca378ac96e9c8e53439dab5fe
mrfaza <mrfaza@d-shore.nl>
2022-08-14 12:18:21 +0700

    create new instances and local config on terraform

--- create-resources-docker/main.tf
+++ create-resources-docker/main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "Terraform Check Tutorial"
+  name  = "terraform-tutorial"
   ports {
     internal = 80
     external = 8000
